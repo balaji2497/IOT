8d4f744dabb431b224f5f009c9fe01ab50def4f9
balaji.c <balaji.c@velocix.com>
2024-07-05 18:05:02 +0530

    IOT lab programs

--- 2_Measuring_temperature.c
+++ 2_Measuring_temperature.c
@@ -6,7 +6,9 @@ void setup()
 void loop() //main loop
 {
 	int analogValue = analogRead(outputpin);
-	float millivolts = (analogValue/1024.0) * 3300; //3300 is the voltage provided by NodeMCU float celsius = millivolts/10;
-	Serial.print("in DegreeC= "); Serial.println(celsius);
+	float millivolts = (analogValue/1024.0) * 3300; //3300 is the voltage provided by NodeMCU
+    float celsius = millivolts/10;
+	Serial.print("in DegreeC= ");
+    Serial.println(celsius);
 	float fahrenheit = ((celsius*9)/5+32); delay(1000);
 }
